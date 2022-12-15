c2f986ae2f5c1b081f123ff6d4c18d304416b0c4
Nikhil-81 <nikhilwalwatkar81@gmail.com>
2022-12-15 14:49:34 +0530

    error_resolve

--- bigbasket/src/App.js
+++ bigbasket/src/App.js
@@ -2,19 +2,12 @@
 import './App.css';
 import AllRoutes from './All-Routes/AllRoutes';
 
-=======
-
->>>>>>> b606b92f86ef49867c6260420e18c15dd5336484
-
-
-
 
 function App() {
   return (
     <div className="App">
-  
   <AllRoutes/>
-  
+
     </div>
   );
 }
